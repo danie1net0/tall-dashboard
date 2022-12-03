8a5419964e186c5180af2a0cb50e6e181e267910
Daniel Neto <daniel@danielneto.dev.br>
2022-12-03 20:39:59 -0300

    Installs and configure the AlpineJS dependency.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+window.livewire.onError(statusCode => {
+  if (statusCode === 419) {
+    window.location.reload();
+
+    return false;
+  }
+});
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,22 +1,13 @@
 import _ from 'lodash';
-window._ = _;
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
+import Alpine from 'alpinejs';
 import axios from 'axios';
-window.axios = axios;
 
+window._ = _;
+window.Alpine = Alpine
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
+Alpine.start();
 
 // import Echo from 'laravel-echo';
 
